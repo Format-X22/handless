22781ac7cb0e51760a9fe3fde3ca90356d4a3cc7
Oleg <oleg.pav.m@gmail.com>
2018-07-06 11:58:30 +0300

    Main + packages.

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,41 @@
-const Control = require('./Control');
-const Strategy = require('./Strategy');
-const logger = require('./Logger');
+const core = require('griboyedov');
+const logger = core.Logger;
+const BasicService = core.service.Basic;
+const Control = require('./service/Control');
+const Strategy = require('./service/Strategy');
+const Bitmex = require('./service/Bitmex');
 
-class Main {
+class Main extends BasicService {
+    constructor() {
+        super();
+
+        this._control = new Control();
+        this._bitmexStrategy = new Strategy(Bitmex);
+        this.addNested(this._bitmexStrategy);
+    }
+
     async start() {
         logger.info('Настройка...');
 
-        const config = await new Control().start();
+        const config = await this._control.start();
 
-        logger.info('Запуск стратегии...');
+        logger.info('Запуск стратегий...');
 
-        await new Strategy(config).start();
+        await this._bitmexStrategy.start(config);
+    }
+
+    async stop() {
+        await this.stopNested();
+        process.exit(0);
     }
 }
 
-(async () => await new Main().start())();
+new Main().start().then(
+    () => {
+        logger.info('Main service started!');
+    },
+    error => {
+        logger.error(`Main service failed - ${error}`);
+        process.exit(1);
+    }
+);
