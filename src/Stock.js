46fd446b28ed6b31c0a51c41789c38aceb213bbd
Oleg <oleg.pav.m@gmail.com>
2018-07-22 16:40:53 +0300

    Stock draft.

--- src/Stock.js
+++ src/Stock.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const request = require('request-promise-native');
 const moment = require('moment');
 const sleep = require('then-sleep');
@@ -6,12 +7,15 @@ const logger = core.Logger;
 
 const MAX_ERRORS = 750;
 const RETRY_TIME = 5000;
+const DOMAIN = 'https://www.bitmex.com';
+const API_POINT = '/api/v1/';
 
 class Stock {
     constructor(publicKey, privateKey) {
         this._requestErrors = 0;
         this._publicKey = publicKey;
         this._privateKey = privateKey;
+        this._positionId = null;
     }
 
     async makeOrder(type, price, amount) {
@@ -24,6 +28,84 @@ class Stock {
 
     async isFlip() {
         // TODO -
+    }
+
+    async _getOrder() {
+        const method = 'GET';
+        const endpoint = 'order';
+        const payload = 'filter={"symbol": "XBTUSD", "open": true}';
+        const expires = this._makeExpires();
+        const sign = this._sign(method, { endpoint, payload, expires });
+
+        const raw = await this._request(
+            method,
+            this._requestUrl(endpoint, payload),
+            this._makeHeaders(sign, expires)
+        );
+
+        return JSON.parse(raw)[0];
+    }
+
+    async _getPosition() {
+        const method = 'GET';
+        const endpoint = 'position';
+        const payload = 'filter={"symbol": "XBTUSD"}';
+        const expires = this._makeExpires();
+        const sign = this._sign(method, { endpoint, payload, expires });
+
+        const raw = await this._request(
+            method,
+            this._requestUrl(endpoint, payload),
+            this._makeHeaders(sign, expires)
+        );
+
+        return JSON.parse(raw)[0];
+    }
+
+    _requestUrl(endpoint, payload = '') {
+        if (payload) {
+            payload = `?${payload}`;
+        }
+
+        return encodeURI(`${DOMAIN}${API_POINT}${endpoint}${payload}`);
+    }
+
+    _sign(method, { endpoint, payload, expires }) {
+        const hmac = crypto.createHmac('sha256', this._privateKey);
+        let data = `${method}${API_POINT}${endpoint}`;
+
+        if (method === 'GET') {
+            data += `?${payload}${expires}`;
+        } else {
+            data += `?${expires}${payload}`;
+        }
+
+        hmac.update(encodeURI(data));
+
+        return hmac.digest('hex');
+    }
+
+    _makeHeaders(sign, expires) {
+        return {
+            'api-expires': expires,
+            'api-key': this._publicKey,
+            'api-signature': sign,
+        };
+    }
+
+    _makeExpires() {
+        return +moment().add(5, 'minutes') / 1000;
+    }
+
+    async _request(method, uri, headers, payload = null) {
+        return await request({
+            method,
+            uri,
+            headers,
+            body: JSON.stringify(payload),
+            json: true,
+            timeout: 5000,
+        });
     }
 }
 
